1f51352a5e4f357760c405cc63ec93f81143f579
Manikanth9808 <manikanthpattar0@gmail.com>
2024-06-20 13:35:30 +0530

    new file

---
+++ Airoplane.java
@@ -0,0 +1,15 @@
+class Airoplane{
+
+
+public static void main(String[] args){
+
+String Airoplane[]={"Airbus A319","0742464639523","mahendra singh","bengaluru","mumbai","900"};
+
+System.out.println("the airoplane name is "+ Airoplane[0]);
+System.out.println("the airoplane id is "+ Airoplane[1]);
+System.out.println("the pilot name is "+ Airoplane[2]);
+System.out.println("the source is "+ Airoplane[3]);
+System.out.println("the Destination is "+ Airoplane[4]);
+System.out.println("the no of seats is" + Airoplane[5]);
+}
+}
