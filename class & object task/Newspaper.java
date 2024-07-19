d5c604bb25db0bbd832d7073a13cf4bf23ac9c57
Manikanth9808 <manikanthpattar0@gmail.com>
2024-07-19 11:13:56 +0530

    obj and class new task

--- class & object task/Newspaper.java
+++ class & object task/Newspaper.java
@@ -1,9 +1,24 @@
-class Newspaper(){
+class Newspaper{
 String companyName;
 int noOfPages;
 String language;
 double price;
-public void  currentAffairs(){
- System.out.println("newspaper gives us a lot of information")
+public boolean  currentAffairs(String companyName,int noOfPages,String language,double price){
+	boolean isNameValid=false;
+	if(companyName!=null && noOfPages>0 && language!=null && price>0.0){
+		this.companyName=companyName;
+		this.noOfPages=noOfPages;
+		this.language=language;
+		this.price=price;
+		isNameValid=true;
+	}
+	return isNameValid;
+}
+public void getDetails(){
+	System.out.println("the company name is "+this.companyName);
+	System.out.println("the no of pages is "+this.noOfPages);
+	System.out.println("the language is "+this.language);
+	System.out.println("the price is "+this.price);
+	System.out.println(" ");
 }
 }
--- class & object task/SimCard.java
+++ class & object task/SimCard.java
@@ -2,11 +2,27 @@ class SimCard{
 String name;
 int id;
 String plan;
+String validity;
 
-public void communication(){
-System.out.println("the Sim card is used for communication");
+public boolean communication(String name,int id,String plan , String validity){
+boolean isSimValid=false;
+if(name!=null && id>0 && plan!=null && validity!=null){
+	this.name=name;
+	this.id=id;
+	this.plan=plan;
+	this.validity=validity;
+	isSimValid=true;
+}
+return isSimValid;
 
 
+}
+public void getDetails(){
+	System.out.println("the sim company is "+this.name);
+	System.out.println("the id number is "+this.id);
+	System.out.println("the plan is "+this.plan);
+	System.out.println("the validity is "+this.validity);
+	
 }
 
 }
